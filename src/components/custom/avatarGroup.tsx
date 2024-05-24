a61d2c441290285fff8eb346d7b01dd1c9c33cea
Neeko <brossard.nicolas09@gmail.com>
2024-05-24 17:07:05 +0200

    minor changes

--- src/components/custom/avatarGroup.tsx
+++ src/components/custom/avatarGroup.tsx
@@ -6,16 +6,16 @@ export const AvatarGroup = () => {
     <div className="flex flex-row">
       <div className="mr-4 flex -space-x-3 *:ring-2 *:ring-white">
         <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-        </Avatar>
-        <Avatar>
-          <AvatarImage src="https://xsgames.co/randomusers/avatar.php?g=male" />
-        </Avatar>
-        <Avatar>
-          <AvatarImage src="https://preview.redd.it/new-gpt-super-avatar-creator-upload-3-selfies-and-get-v0-veruy5hcsidc1.jpg?width=1024&format=pjpg&auto=webp&s=c2d20b635ccc9874ef87e385b6d20fdbdae4e849" />
+          <AvatarImage src="assets/avatar3.png" />
         </Avatar>
         <Avatar>
           <AvatarImage src={"assets/avatar2.png"} />
+        </Avatar>
+        <Avatar>
+          <AvatarImage src="assets/avatar5.png" />
+        </Avatar>
+        <Avatar>
+          <AvatarImage src="assets/avatar6.png" />
         </Avatar>
         <Avatar>
           <AvatarImage src={"assets/avatar1.png"} />
